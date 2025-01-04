5f86c7fb49328a6e0cb21e43c03f6ae10cefea46
Vinayak Paka <vinayakpaka22@gmail.com>
2025-01-04 16:20:28 +0530

    kanban Project Files

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        "columnId":0,
+        "tasks":[
+            {"taskId":97522, "content": "To learn JavaScript"},
+            {"taskId":7243, "content":"To build kanban Project"}
+        ]
+    },
+    {
+        "columnId":1,
+        "tasks":[
+            {"taskId":38833, "content": "Machine Learning Notes"}
+        ]
+    },
+    {
+        "columnId":2,
+        "tasks":[
+            {"taskId":88198, "content": "JavaScript Basic Projects"},
+            {"taskId":23030, "content":"Edit Videos"}
+        ]
+    }
+]
